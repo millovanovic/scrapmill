a1b119b3466553404ea8de1c65219b20e636c56a
millovanovic <bluesboba@gmail.com>
2018-04-23 12:59:53 +0200

    Avanza session implemented.

--- scrapmill.py
+++ scrapmill.py
@@ -1,11 +1,33 @@
 import csv
 import re
+import os
 import requests as req
-import os.path as osp
 import datetime as dt
 import time as tm
 from bs4 import BeautifulSoup as bs
 
+
+class Session:
+    """A simple web session class."""
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36',
+               'Cache-Control': 'private, max-age=0, no-cache'}
+    def __init__(self, name, url, usr, pwd):
+        self.name = name
+        self.sess = req.Session()
+        self.url = url
+        self.usr = usr
+        self.pwd = pwd
+
+    def login(self):
+        form = {'j_username': self.usr, 'j_password': self.pwd}
+        request = self.sess.post(self.url + '/ab/handlelogin', data=form)
+
+        if request.status_code != req.codes.ok:
+            print("Login failed.")
+            return False
+
+        print("Login successful.")
+        return True
 
 class Asset:
     """A simple asset class."""
@@ -18,15 +40,20 @@ class Asset:
         self.time = []
         self.source = []
 
-    def download_data(self, source):
-        html_data = bs(req.get(source).content, 'html.parser')
+    def download_data(self, source, Session):
+        html_data = bs(Session.sess.get(source, headers=Session.HEADERS).content, 'html.parser')
         print(str(dt.datetime.now()) + ": Data downloaded.")
+
+        html_data = html_data.find(class_='cleanList floatList clearFix quoteBar ')
 
         buy_html = str(html_data.find_all(
             class_='buyPrice SText bold')).replace(',', '.')
+        print buy_html
         sell_html = str(html_data.find_all(
             class_='sellPrice SText bold')).replace(',', '.')
+        print sell_html
         time_html = str(html_data.find_all(class_='updated SText bold'))
+        print time_html
 
         buy_price = float(re.findall('">(.*)</', buy_html)[0])
         sell_price = float(re.findall('">(.*)</', sell_html)[0])
@@ -52,8 +79,15 @@ def working_hours(current, opening, closing):
 
 
 if __name__ == '__main__':
-    # webpage where live data can be found
-    source = "https://www.avanza.se/borshandlade-produkter/warranter-torg/om-warranten.html/718871/mini-l-tesla-ava-28"
+    url = 'https://www.avanza.se'
+    usr = os.environ['AVA_USR'] #assuming that credentials are saved in environment variables
+    pwd = os.environ['AVA_PWD'] #assuming that credentials are saved in environment variables
+
+    avanza = Session('Avanza', url, usr, pwd)
+    avanza.login()
+
+
+    source = "https://www.avanza.se/borshandlade-produkter/warranter-torg/om-warranten.html/718871/mini-l-tesla-ava-28" # webpage where
     path = './TSLA.csv'  # local path to store data
     tesla = Asset("TSLA")  # initiate asset instance
 
@@ -67,14 +101,14 @@ if __name__ == '__main__':
     while scrapping_status:
         current_local_time = dt.datetime.now()
         if working_hours(current_local_time, exchange_opening_time, exchange_closing_time):
-            tesla.download_data(source)
+            tesla.download_data(source, avanza)
             tesla.write_data(path)
             tm.sleep(scrape_freq)
 
         else:
-            print("Now it is " + \
-                current_local_time.strftime(
-                    '%A, %x, %X') + " and the market is closed. The scrapping will continue as soon as the market opens.")
+            print("Now it is " +
+                  current_local_time.strftime(
+                      '%A, %x, %X') + " and the market is closed. The scrapping will continue as soon as the market opens.")
             current_plus_hour = current_local_time + dt.timedelta(hours=1)
             current_plus_minute = current_local_time + dt.timedelta(minutes=1)
             if working_hours(current_plus_minute, exchange_opening_time, exchange_closing_time):
